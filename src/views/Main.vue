e1b95526db0002e21c852448b083e931f87b46f8
luc41 <whthunder@live.cn>
2020-06-11 16:32:57 +0800

    add:tables

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page
-    class="flex q-pa-md"
+    class="q-pa-md"
     style="padding-top: 60px"
   >
     <q-page-sticky
@@ -13,38 +13,79 @@
       </q-toolbar>
     </q-page-sticky>
 
-    <div class="column">
-      <div class="col q-gutter-sm">
-        <spec-filter />
+    <div class="row q-gutter-sm">
+      <div class="col-auto">
+        <div class="column">
+          <div class="col q-gutter-sm">
+            <spec-filter />
 
-        <level-filter />
+            <level-filter />
 
-        <gear-filter />
+            <gear-filter />
 
-        <q-btn
-          color="positive"
-        >
-          <q-icon
-            left
-            name="sync"
-          />
-          <span>Apply Filter</span>
-        </q-btn>
-        <q-btn
-          color="negative"
-        >
-          <q-icon
-            left
-            name="functions"
-          />
-          <span>Calculate</span>
-        </q-btn>
+            <q-btn
+              color="positive"
+            >
+              <q-icon
+                left
+                name="sync"
+              />
+              <span>Apply Filter</span>
+            </q-btn>
+
+            <q-btn
+              color="negative"
+            >
+              <q-icon
+                left
+                name="functions"
+              />
+              <span>Calculate</span>
+            </q-btn>
+          </div>
+        </div>
       </div>
       <div class="col">
-        <img
-          alt="Quasar logo"
-          src="../assets/logo.png"
+        <q-scroll-area
+          style="height:100%;"
+          :delay="1200"
         >
+          <q-table
+            title="head"
+            dense
+            card-class="bg-grey-4"
+            :data="headData"
+            :columns="columns"
+            row-key="name"
+            selection="single"
+            :selected.sync="selectedHead"
+            :loading="loadingHead"
+            virtual-scroll
+            :pagination.sync="pagination"
+            :rows-per-page-options="[0]"
+          />
+          <div class="q-mt-md">
+            Selected: {{ JSON.stringify(selectedHead) }}
+          </div>
+
+          <q-table
+            title="head"
+            dense
+            card-class="bg-grey-4"
+            :data="headData"
+            :columns="columns"
+            row-key="name"
+            selection="single"
+            :selected.sync="selectedHead"
+            :loading="loadingHead"
+            virtual-scroll
+            :pagination.sync="pagination"
+            :rows-per-page-options="[0]"
+          />
+          <div class="q-mt-md">
+            Selected: {{ JSON.stringify(selectedHead) }}
+          </div>
+        </q-scroll-area>
       </div>
     </div>
   </q-page>
@@ -64,6 +105,131 @@ export default {
   },
   data () {
     return {
+      selectedHead: [],
+      loadingHead: true,
+      pagination: {
+        rowsPerPage: 0
+      },
+      columns: [
+        {
+          name: 'desc',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+        { name: 'protein', label: 'Protein (g)', field: 'protein' },
+        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+        { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+        { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+      ],
+      headData: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129,
+          calcium: '8%',
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          sodium: 337,
+          calcium: '6%',
+          iron: '7%'
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          sodium: 413,
+          calcium: '3%',
+          iron: '8%'
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          sodium: 327,
+          calcium: '7%',
+          iron: '16%'
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          sodium: 50,
+          calcium: '0%',
+          iron: '0%'
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          sodium: 38,
+          calcium: '0%',
+          iron: '2%'
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          sodium: 562,
+          calcium: '0%',
+          iron: '45%'
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          sodium: 326,
+          calcium: '2%',
+          iron: '22%'
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          sodium: 54,
+          calcium: '12%',
+          iron: '6%'
+        }
+      ]
     }
   }
 }
