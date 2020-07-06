5ba7e3fb2f9b7d7145503300a6ab5c8cce90138f
luc41 <whthunder@live.cn>
2020-07-06 14:35:51 +0800

    add response interceptor

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,22 @@
 import Axios from 'axios'
+import store from '../store'
 
 const service = Axios.create({
   baseURL: 'https://cafemaker.wakingsands.com',
   timeout: 99999
 })
+
+// http response interceptor
+service.interceptors.response.use(
+  function (response) {
+    if (response.data.code === 0 || response.headers.success === 'true') {
+      return response.data
+    } else {
+      store.commit('changeLoadingState')
+      return Promise.reject(response.data.msg)
+    }
+  }, function (error) {
+    return Promise.reject(error)
+  })
 
 export default service
