93fdf0cbb50cabb3db16d4c85915e12b0f7b8429
luc41 <whthunder@live.cn>
2020-07-07 15:58:26 +0800

    finish cascader

--- src/components/specFilter.vue
+++ src/components/specFilter.vue
@@ -1,9 +1,9 @@
 <template>
   <q-card class="bg-info">
     <q-card-section horizontal>
-      <q-card-section>
+      <q-card-section class="q-pr-none">
         <q-select
-          style="width: 8em;"
+          style="width: 9em;"
           standout="bg-info text-white"
           v-model="raciesModel"
           :loading="raceSelectLoadingState"
@@ -18,15 +18,16 @@
         />
       </q-card-section>
 
-      <q-card-section>
+      <q-card-section class="q-pl-xs">
         <q-select
           style="width: 12em;"
           standout="bg-info text-white"
           v-model="clansModel"
           :loading="clanSelectLoadingState"
+          :disable="disableClanSelect"
           transition-show="jump-up"
           transition-hide="jump-down"
-          :options="$store.state.clansStorage"
+          :options="cascader"
           option-label="Name"
           option-value="Name"
           emit-value
@@ -91,11 +92,32 @@ export default {
       raciesModel: null,
       raceSelectLoadingState: true,
       clansModel: null,
+      cascader: null,
       clanSelectLoadingState: true,
+      disableClanSelect: true,
       levelSlider: 80
     }
   },
   methods: {
+  },
+  watch: {
+    raciesModel (val) {
+      this.clansModel = null
+      if (val) {
+        this.disableClanSelect = false
+      }
+      let index = 0
+      for (const i in this.$store.state.raciesStorage) {
+        switch (val) {
+          case this.$store.state.raciesStorage[i].Name:
+            this.cascader = this.$store.state.clansStorage.slice(2 * index, 2 * (index + 1))
+            break
+          default:
+            break
+        }
+        index += 1
+      }
+    }
   },
   mounted () {
     this.$axios
